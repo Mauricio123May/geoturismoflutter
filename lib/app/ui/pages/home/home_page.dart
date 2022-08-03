0adee1b8636d58f94615fceb0156cb51cc6affdb
Mauricio May Pech <191k0059@itscarrillopuerto.edu.mx>
2022-08-03 11:19:27 -0500

    Primer Commit

---
+++ lib/app/ui/pages/home/home_page.dart
@@ -0,0 +1,35 @@
+
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final _initialCameraPosition = CameraPosition(
+    target: LatLng(0,0),
+    );
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: GoogleMap(
+        initialCameraPosition: _initialCameraPosition,
+        ),
+    );
+    
+  }
+}
